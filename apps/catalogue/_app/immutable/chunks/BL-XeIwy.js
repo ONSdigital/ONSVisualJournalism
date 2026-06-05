5930a6de963fcf19799db524ae3ad30903244f77
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-06-05 10:19:35 +0000

    Built site for gh-pages

---
+++ apps/catalogue/_app/immutable/chunks/BL-XeIwy.js
@@ -0,0 +1,2 @@
+//js
+import{h as r,d as n,e as u}from"./DInywlPj.js";function _(i,f,t,l,s){var d;r&&n();var e=(d=f.$$slots)==null?void 0:d[t],a=!1;e===!0&&(e=f[t==="default"?"children":t],a=!0),e===void 0?s!==null&&s(i):e(i,a?()=>l:l)}u();export{_ as s};
---
+++ apps/catalogue/_app/immutable/nodes/0.CC1KVH4D.js
@@ -0,0 +1,2 @@
+//js
+import{c as a,a as n}from"../chunks/C7l7Tchu.js";import{s as l}from"../chunks/BL-XeIwy.js";import{f as s}from"../chunks/DInywlPj.js";const i=!0,f="always",u=Object.freeze(Object.defineProperty({__proto__:null,prerender:i,trailingSlash:f},Symbol.toStringTag,{value:"Module"}));function d(o,t){var e=a(),r=s(e);l(r,t,"default",{},null),n(o,e)}export{d as component,u as universal};
